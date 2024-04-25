ff8ee9ce2838ffd598d2638b28aad35c6d9ddf4e
Subodh <subodhacharya21@gmail.com>
2024-04-25 23:53:35 +0545

    changed apostrophe

--- src/app/dashboard/campaigns/page.js
+++ src/app/dashboard/campaigns/page.js
@@ -77,7 +77,7 @@ const Page = () => {
         Objective
       </label>
       <textarea name='objective' value={vars.objective} onChange={(e)=>{changeField(e)}} rows={5} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password"/>
-      <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+      <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you would like</p>
     </div>
     <div className="w-full md:w-1/2 px-3">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
