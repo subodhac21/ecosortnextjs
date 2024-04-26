1b837f7eafc0a30804f1f4bd7dc31bc419b4bd71
Subodh <subodhacharya21@gmail.com>
2024-04-26 16:46:22 +0545

    added for mobile

--- src/app/components/leftbar.js
+++ src/app/components/leftbar.js
@@ -193,11 +193,11 @@ export default function Example({children}) {
                         </li>
                         <li className="mt-auto">
                           <Link
-                            href="#"
+                            href="/dashboard/checkgarbage"
                             className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
                           >
                             <Cog6ToothIcon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                            Settings
+                            Check Garbage
                           </Link>
                         </li>
                       </ul>
